d19bc414487977c49680b48c4b9bb70f1c623e45
Higashi-Kota <higashi.kota@gri.jp>
2022-07-29 16:12:26 +0900

    add sample

--- react-vite-app/src/components/Item.jsx
+++ react-vite-app/src/components/Item.jsx
@@ -1,21 +1,35 @@
 import {css} from '@emotion/css';
 import {AnimatePresence, motion, useAnimation} from 'framer-motion';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {default as Layout} from '../layouts/default';
+import {RiDeleteBin5Line} from 'react-icons/ri';
 
 const Item = ({total, index, onDelete, data}) => {
   const controls = useAnimation();
-
+  const [isDelete, setIdDelete] = useState(false);
   const handleDragEnd = async (event, info) => {
     const offset = info.offset.x;
     const velocity = info.velocity.x;
-
-    if (offset < -100 || velocity < -500) {
+    console.log(offset);
+    if (offset < -150) {
+      console.log(`a`);
       await controls.start({x: '-100%', transition: {duration: 0.2}});
       onDelete(index);
+    } else if (-150 < offset && offset < 0) {
+      console.log(`b`);
+      await controls.start({
+        x: `-30%`,
+        opacity: 1,
+        transition: {duration: 0.5},
+      });
+      setIdDelete(true);
+    } else if (0 < offset && offset < 150) {
+      console.log(`c`);
+      await controls.start({x: `0%`, opacity: 1, transition: {duration: 0.5}});
+      setIdDelete(false);
     } else {
-      // console.log(`a`);
-      // await controls.start({x: `-30%`, opacity: 1, transition: {duration: 0.5}});
+      console.log(`d`);
+      setIdDelete(false);
       await controls.start({x: `0%`, opacity: 1, transition: {duration: 0.5}});
     }
   };
@@ -35,47 +49,69 @@ const Item = ({total, index, onDelete, data}) => {
       transition={{type: 'spring', stiffness: 600, damping: 30}}
     >
       <Layout>
-        <motion.div
+        <div
           className={css`
             width: 100%;
             min-height: 70px;
-            background-color: #fff;
             display: flex;
-            justify-content: center;
+            justify-content: space-evenly;
             align-items: center;
-            gap: 1rem;
-            position: relative;
-            ::after {
-              content: 'a';
-              position: absolute;
-              width: 70px;
-              height: 70px;
-              top: 0;
-              right: 0;
-            }
           `}
-          drag="x"
-          dragDirectionLock
-          onDragEnd={handleDragEnd}
-          animate={controls}
         >
-          <motion.img
-            src={data.url}
-            width={30}
+          <motion.div
             className={css`
-              flex-basis: 30%;
-            `}
-            alt={``}
-          />
-          <p
-            className={css`
-              flex-basis: 70%;
               padding: 0.5rem;
+              width: 100%;
+              height: 100%;
+              background-color: #fff;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              gap: 1rem;
+              position: relative;
+              margin-right: ${isDelete ? '-30%' : 'initial !important'};
             `}
+            drag="x"
+            dragDirectionLock
+            onDragEnd={handleDragEnd}
+            animate={controls}
           >
-            {data.description}
-          </p>
-        </motion.div>
+            <motion.img
+              src={data.url}
+              width={30}
+              className={css`
+                flex-basis: 30%;
+              `}
+              alt={``}
+            />
+            <p
+              className={css`
+                flex-basis: 70%;
+                padding: 0.5rem;
+              `}
+            >
+              {data.description}
+            </p>
+          </motion.div>
+          <div
+            className={css`
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              width: ${isDelete ? 'auto' : 0};
+              transition: width 0.6s ease-in-out;
+              &:hover {
+                cursor: pointer;
+              }
+            `}
+            onClick={async (e) => {
+              await controls.start({x: '-100%', transition: {duration: 0.2}});
+              onDelete(index);
+            }}
+          >
+            <RiDeleteBin5Line size={40} />
+          </div>
+        </div>
       </Layout>
     </motion.div>
   );
